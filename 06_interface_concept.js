df501da653cd734be1a36e3d5d415b2f5eabef79
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-07 21:31:25 +0530

    interface concepts

--- 06_interface_concept.js
+++ 06_interface_concept.js
@@ -1,7 +1,6 @@
 export {}
 
 // interface
-
 interface Person { 
   firstName : string,
   lastName : string
@@ -10,3 +9,25 @@ interface Person {
 function fullName ( person: Person)  {
   return `${person.firstName} ${person.lastName}`
 }
+
+interface Employee {
+  firstName : string,
+  lastName? : string,
+  position : string,
+  salary : number
+}
+
+function Details ( person : Employee ){
+  return `Name : ${person.firstName} ${person.lastName}
+  \nPosition : ${person.position}
+  \nSalary: ${person.salary}`
+}
+
+const obj = {
+  firstName : "Ramesh",
+  // lastName : "Roy",
+  position : "Software Developer",
+  salary: 240000
+}
+
+console.log(Details(obj))
