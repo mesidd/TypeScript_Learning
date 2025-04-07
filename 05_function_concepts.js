fe2f387db548b91c998d43c04c6a22fe3a876640
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-07 20:53:41 +0530

    function

--- 05_function_concepts.js
+++ 05_function_concepts.js
@@ -14,3 +14,23 @@ function multiply (num1 : number, num2: number, num3 : number = 10 ): number{
   return num1 * num2 * ( num3 || 3 )
 }
 console.log(multiply( 4, 3 ))
+
+// function fullName ( person: {firstName: string, lastName: string} ) : string {
+//   return (person.firstName + person.lastName)
+// }
+
+function fullName ( person: {firstName: string, lastName: string} ) : string {
+  return (
+    `${person.firstName.charAt(0).toUpperCase() + person.firstName.slice(1)} 
+    ${person.lastName.charAt(0).toUpperCase() + person.lastName.slice(1)}`
+  )
+}
+
+const obj = {
+  firstName : "ramesh",
+  lastName : 'roy'
+}
+console.log(fullName(obj));
+
+
+
