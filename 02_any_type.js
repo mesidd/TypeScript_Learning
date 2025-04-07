7969e8916a4c1422608e31780dde3229f9ffa515
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-07 19:08:37 +0530

    unknown type

---
+++ 02_any_type.js
@@ -0,0 +1,20 @@
+// any data type
+// all property valid
+let myVariable : any = 10;
+myVariable.toLowercase();
+myVariable.toString();
+myVariable.length;
+
+// unknown data type 
+// require type casting
+let newVariable : unknown = 'ram';
+
+// newVariable.toLowercase(); // Error 
+(newVariable as string).toLowerCase();
+
+// newVariable + 10
+(newVariable as number) + 10;
+
+// *** array as []
+// (newVariable as Array).length // wrong
+(newVariable as []).length
