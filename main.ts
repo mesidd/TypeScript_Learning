57ba29b907299d97d6a1d27c2e383c2821ed9aa2
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-07 18:52:19 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+export {}
+let message = 'Welcome world'
+console.log(message);
+
+let isBeginner: boolean = true;
+let name: string = "Sid Roy";
+
+let salary: number = 200000
+// place = 'Miami'
+let bio: string = `Hey my name is ${name} I earn $${salary} 
+Am i a beginner - ${isBeginner}`
+
+let n: null = null;
+let u: undefined = undefined;
+
+// let isNew: boolean = null; // wrong
+// let myName: string = undefined;  // wrong
+
+console.log(bio)
+
+let person1: [string, number] = ['Chris', 23];
+
+enum color { a = 'Red', b = 'Green' , c = 'Blue' };
+
+let e : color = color.a;
+let d : color = color.b;
+let c : color = color.c;
+
+console.log(person1)
+
+console.log(e,d,c)
+
+
+let randomValue : any = 10
+randomValue = true;
+randomValue = 'mesidd'
+console.log(randomValue)
